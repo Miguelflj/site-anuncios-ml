c01edf9e75264b2d63e586e8f9b1c3986db706c9
Miguel Freitas <miguelflj@gmail.com>
2022-05-05 16:01:27 -0300

    add mensage to not ad, and create path to ads

--- ml_anuncios/urls.py
+++ ml_anuncios/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 
 
-from .views import home
+from .views import anuncio, categoria, home
 
 urlpatterns = [
-     path('', home, name='home')
+     path('', home, name='home'),
+     path('categoria/<int:categoria_id>', categoria, name='categoria'),
+     path('anuncio/<int:anuncio_id>', anuncio, name='anuncio')
+     
 ]   
--- ml_anuncios/views.py
+++ ml_anuncios/views.py
@@ -1,3 +1,5 @@
+from unicodedata import category
+from django.shortcuts import get_object_or_404
 from django.shortcuts import render
 from django.http import HttpResponse
 
@@ -10,3 +12,19 @@ def home(request):
     last_ads = Ad.objects.all()[:12]
  
     return render(request, 'home.html', {'categorias': categories, 'anuncios':last_ads})
+
+
+def categoria(request,categoria_id):
+    categoria = get_object_or_404(Category, id=categoria_id)
+    categories = Category.objects.all()
+    
+    
+    ads = Ad.objects.filter(category=categoria)
+    
+    return render(request, 'home.html', {'categorias': categories, 'anuncios':ads, 'categoria':categoria})
+
+
+def anuncio(request,anuncio_id):
+    anuncio = get_object_or_404(Ad, id=anuncio_id)
+    
+    return render(request, 'home.html', {'categorias': categories, 'anuncios':ads, 'categoria':categoria})
